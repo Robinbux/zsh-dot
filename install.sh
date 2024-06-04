adce8996992f7465b0ecbcc621500cc812f4abbe
Robinbux <robinbux@gmail.com>
2024-06-04 14:21:23 +0200

    p10k

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ TMUX_VERSION="3.3a"
 TMUX_SOURCE_URL="https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz"
 POWERLEVEL10K_REPO="https://github.com/romkatv/powerlevel10k.git"
 LAZYVIM_REPO="https://github.com/LazyVim/starter"
+OH_MY_ZSH_REPO="https://github.com/ohmyzsh/ohmyzsh.git"
+OH_MY_ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 
 install_dependencies() {
     echo "Installing dependencies..."
@@ -35,10 +37,22 @@ install_tmux() {
     rm -rf tmux-${TMUX_VERSION} tmux.tar.gz
 }
 
+install_oh_my_zsh() {
+    echo "Installing Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    #sed -i 's|ZSH_THEME=".*"|ZSH_THEME="powerlevel10k/powerlevel10k"|' "$HOME/.zshrc"
+}
+
 install_powerlevel10k() {
     echo "Installing Powerlevel10k..."
-    git clone --depth=1 "${POWERLEVEL10K_REPO}" "${HOME}/.powerlevel10k"
-    echo 'source ~/.powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+    git clone --depth=1 "${POWERLEVEL10K_REPO}" "${OH_MY_ZSH_CUSTOM}/themes/powerlevel10k"
+    # Add Powerlevel10k configuration to .zshrc
+    cat <<EOF >> "$HOME/.zshrc"
+# To customize Powerlevel10k, edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+EOF
+    # Copy the .p10k.zsh file from the repository
+    cp "${CONFIG_DIR}/.p10k.zsh" "$HOME/.p10k.zsh"
 }
 
 install_lazyvim() {
@@ -48,15 +62,15 @@ install_lazyvim() {
 
 setup_config() {
     echo "Setting up configuration directory..."
-    mkdir -p "${CONFIG_DIR}"
+    mkdir -p "${SCRIPT_DIR}"
 
     # Symlink configuration files
-    ln -sf "${CONFIG_DIR}/.tmux.conf" "$HOME/.tmux.conf"
+    ln -sf "${SCRIPT_DIR}/.tmux.conf" "$HOME/.tmux.conf"
 
     # Source all .zsh files in the repository
     cat <<EOF >> "$HOME/.zshrc"
 # Source custom Zsh configurations
-for file in ${CONFIG_DIR}/*.zsh; do
+for file in ${SCRIPT_DIR}/.zsh/*.zsh; do
     source \$file
 done
 EOF
@@ -66,6 +80,7 @@ EOF
 main() {
     install_dependencies
     install_tmux
+    install_oh_my_zsh
     setup_config
     install_powerlevel10k
     install_lazyvim
