31ddde5fa6e426cf06a479301ba0836999fa2868
Robinbux <robinbux@gmail.com>
2024-06-17 14:36:05 +0200

    non-interactive brew

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ install_dependencies() {
 
 install_homebrew() {
     echo "Installing Homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+     yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 }
 
 install_rust() {
